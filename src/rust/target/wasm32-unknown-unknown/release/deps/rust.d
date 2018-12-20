f39380a60ea0e688b073f78fe6cfe0dda61419ad
Chris Pikul <>
2018-12-20 23:39:26 +0300

    Moved to docs for github pages

---
+++ src/rust/target/wasm32-unknown-unknown/release/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\deps\rust.wasm: src\lib.rs
+
+C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\deps\rust.d: src\lib.rs
+
+src\lib.rs:
---
+++ src/rust/target/wasm32-unknown-unknown/release/rust.d
@@ -0,0 +1,1 @@
+C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\rust.wasm: C:\Dev\chromaview\src\rust\src\lib.rs
