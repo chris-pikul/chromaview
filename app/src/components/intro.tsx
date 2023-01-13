3614df32bbd79444f9737c35cef7b6f9e39aec2f
Chris Pikul <chris@workmaple.com>
2023-01-13 09:03:24 -0800

    some intro copy tests

--- app/src/components/intro.tsx
+++ app/src/components/intro.tsx
@@ -1,6 +1,7 @@
 import Hero from './intro-hero';
 
 import type { ComponentProps } from 'react';
+import NoBr from './nobr';
 
 export interface IntroProps {
   onDismiss: Function;
@@ -11,5 +12,13 @@ export default function Intro({
 }:IntroProps) {
   return <header id='intro' onClick={() => onDismiss()}>
     <Hero />
+    <p>Ever wondered what the world looked like through someone else's eyes?
+    Maybe you know someone with a color-blind condition, and you yourself having
+    normal vision, have been curious how the world might look to them?</p>
+    <p>This application can use your device's camera to apply a filter to the
+    world around you, presenting you with an aproximation of color-blindness.</p>
+    <p><em>Disclaimer:</em> Color, and vision for that matter, is subjective. I
+    cannot promise this is exactly how the world looks under color-blindness.
+    Nor should you take any interpretations from this as medical diagnosis.</p>
   </header>
 }
