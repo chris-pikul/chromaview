5743e8b9bc2a05d7af058b21a1a8e07d14f52864
Chris Pikul <chris@workmaple.com>
2023-01-13 10:32:21 -0800

    content wrapping for oversized monitors

--- app/src/components/intro.tsx
+++ app/src/components/intro.tsx
@@ -11,17 +11,19 @@ export default function Intro({
   transition,
 }:IntroProps) {
   return <header id='intro' className={transition ? 'transition' : ''}>
-    <Hero onStart={() => onDismiss()} />
+    <div className='content-wrap'>
+      <Hero onStart={() => onDismiss()} />
 
-    <p>Ever wondered what the world looked like through someone else's eyes?
-    Maybe you know someone with a color-blind condition, and you yourself having
-    normal vision, have been curious how the world might look to them?</p>
+      <p>Ever wondered what the world looked like through someone else's eyes?
+      Maybe you know someone with a color-blind condition, and you yourself having
+      normal vision, have been curious how the world might look to them?</p>
 
-    <p>This application can use your device's camera to apply a filter to the
-    world around you, presenting you with an aproximation of color-blindness.</p>
+      <p>This application can use your device's camera to apply a filter to the
+      world around you, presenting you with an aproximation of color-blindness.</p>
 
-    <p><em>Disclaimer:</em> Color, and vision for that matter, is subjective. I
-    cannot promise this is exactly how the world looks under color-blindness.
-    Nor should you take any interpretations from this as medical diagnosis.</p>
+      <p className='sub'><em>Disclaimer:</em> Color, and vision for that matter, is subjective. I
+      cannot promise this is exactly how the world looks under color-blindness.
+      Nor should you take any interpretations from this as medical diagnosis.</p>
+    </div>
   </header>
 }
