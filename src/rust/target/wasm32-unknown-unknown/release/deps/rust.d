c7ee1135b267a64e06dd00a7710439a1eadc5163
Chris Pikul <chris@workmaple.com>
2023-01-13 05:43:05 -0800

    clearing out for version 2

--- src/rust/target/wasm32-unknown-unknown/release/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\deps\rust.wasm: src\lib.rs
-
-C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\deps\rust.d: src\lib.rs
-
-src\lib.rs:
--- src/rust/target/wasm32-unknown-unknown/release/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Dev\chromaview\src\rust\target\wasm32-unknown-unknown\release\rust.wasm: C:\Dev\chromaview\src\rust\src\lib.rs
