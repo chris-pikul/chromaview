a089347b57da736398cadfd499cd74916007b050
Chris Pikul <chris@workmaple.com>
2023-02-02 16:39:04 -0800

    change vite config to relative to docs for GH

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/chromaview/',
   plugins: [react()],
-})
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
+});
