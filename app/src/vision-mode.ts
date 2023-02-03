572f175e2a3ed109696becec7f09cda2bf1fd8be
Chris Pikul <chris@workmaple.com>
2023-02-02 16:40:50 -0800

    make paths relative

--- app/src/vision-mode.ts
+++ app/src/vision-mode.ts
@@ -7,45 +7,45 @@ export type VisionMode = {
 export const VisionModes:Record<string, VisionMode> = {
   achromatomaly: {
     name: 'Achromatomaly',
-    url: '/LUTs/achromatomaly.lut.png'
+    url: './LUTs/achromatomaly.lut.png'
   },
   achromatopsia: {
     name: 'Achromatopsia',
-    url: '/LUTs/achromatopsia.lut.png'
+    url: './LUTs/achromatopsia.lut.png'
   },
   deuteranomaly: {
     name: 'Deuteranomaly',
-    url: '/LUTs/deuteranomaly.lut.png'
+    url: './LUTs/deuteranomaly.lut.png'
   },
   deuteranopia: {
     name: 'Deuteranopia',
-    url: '/LUTs/deuteranopia.lut.png'
+    url: './LUTs/deuteranopia.lut.png'
   },
   protanomaly: {
     name: 'Protanomaly',
-    url: '/LUTs/protanomaly.lut.png'
+    url: './LUTs/protanomaly.lut.png'
   },
   protanopia: {
     name: 'Protanopia',
-    url: '/LUTs/protanopia.lut.png'
+    url: './LUTs/protanopia.lut.png'
   },
   tritanomaly: {
     name: 'Tritanomaly',
-    url: '/LUTs/tritanomaly.lut.png'
+    url: './LUTs/tritanomaly.lut.png'
   },
   tritanopia: {
     name: 'Tritanopia',
-    url: '/LUTs/tritanopia.lut.png'
+    url: './LUTs/tritanopia.lut.png'
   },
 
   canine: {
     name: 'Canine (Dog)',
-    url: '/LUTs/animals/canine.lut.png',
+    url: './LUTs/animals/canine.lut.png',
     acuityDegrade: 3,
   },
   feline: {
     name: 'Feline (Cat)',
-    url: '/LUTs/animals/feline.lut.png',
+    url: './LUTs/animals/feline.lut.png',
     acuityDegrade: 2,
   },
 };
